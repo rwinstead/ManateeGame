88d1c53cf182479f3f49f7bd72dace18fbec7f1d
Ryan Winstead <rwinstead6@gatech.edu>
2021-01-03 18:13:17 -0500

    reset on water hit

---
+++ Assets/Scripts/KillPlayerOnCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayerOnCollision : MonoBehaviour
+{
+
+    public Transform startPosition;
+    public GameObject Player;
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = Player.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("triggered");
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log(Player.transform.position);
+            Debug.Log(startPosition.position);
+            cc.enabled = false;
+            Player.transform.position = startPosition.position;
+            cc.enabled = true;
+        }
+
+
+
+    }
+
+
+}
