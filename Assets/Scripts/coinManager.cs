87aaa3ed3ae8c6b60bcdda7196caaa2fb0116678
foxencore <foxencore@users.noreply.github.com>
2021-01-11 16:33:35 -0600

    Added Goals & Coin Collectables

---
+++ Assets/Scripts/coinManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinManager : MonoBehaviour
+{
+
+    private int coinsCollected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinsCollected = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            coinsCollected++;
+            Debug.Log("Got Coin! New Total: "+coinsCollected);
+            other.gameObject.SetActive(false);
+        }
+    }
+}
