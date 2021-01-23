c5e2911f90773b732cdb6171728b81fc255952fc
foxencore <foxencore@users.noreply.github.com>
2021-01-23 16:31:51 -0600

    Added Racing02 - very broken + unfinished

---
+++ Assets/Scripts/Level02/SpinnersFast.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnersFast : MonoBehaviour
+{
+
+    private float speed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.Rotate(0f, speed, 0f);
+    }
+}
