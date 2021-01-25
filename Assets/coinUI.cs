d89f206e7f0a0bd4ba920a833bda83363ec3b519
rwinstead6 <62908528+rwinstead6@users.noreply.github.com>
2021-01-24 19:12:09 -0500

    added bounce, some basic player collission, and coins are networked

---
+++ Assets/coinUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class coinUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coinCountText;
+
+    private int coinCount = 0;
+    private void OnEnable()
+    {
+        coinManager.collectCoin += updateCoinCount;
+        coinCountText.text = "Coins: 0";
+    }
+
+    private void updateCoinCount()
+    {
+
+        coinCount++;
+        coinCountText.text = "Coins: " + coinCount;
+
+    }
+
+
+}
