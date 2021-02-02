e0ad4f015b61d765e6c9aeb46a97b9f20194ee00
firequake <firequake@users.noreply.github.com>
2021-02-01 21:27:18 -0600

    Fixed GoalTriggerEvent Bug

--- Assets/Scripts/GoalTriggerEvent.cs
+++ Assets/Scripts/GoalTriggerEvent.cs
@@ -25,16 +25,7 @@ public class GoalTriggerEvent : MonoBehaviour
         
         if (other.gameObject.tag == "Player")
         {
-            Debug.Log("Goal Reached!");
-
-            //Debug.Log(Player.transform.position);
-            Debug.Log(startPosition.position);
-
-
-            other.gameObject.transform.position = startPosition.position;
-            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            other.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
+            other.GetComponentInParent<PlayerRespawn>().Respawn();
         }
 
 
