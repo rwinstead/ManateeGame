491c77c36832e1a8229756c8153800aa4215ed5d
firequake <firequake@users.noreply.github.com>
2021-02-10 19:48:26 -0600

    moved prefab folder back

--- Assets/Scripts/Speedometer.cs
+++ Assets/Scripts/Speedometer.cs
@@ -8,20 +8,23 @@ using TMPro;
 public class Speedometer : NetworkBehaviour
 {
     private TMP_Text speedometerText;
-    private Rigidbody PlayerRB;
+    public Rigidbody PlayerRB;
 
 
     // Start is called before the first frame update
     void Start()
     {
         speedometerText = GameObject.Find("SpeedometerUI").GetComponent<TMP_Text>();
-            PlayerRB = GetComponentInParent<Rigidbody>();
-
+           
     }
 
     // Update is called once per frame
     void Update()
     {
-            speedometerText.text = string.Format("Speed: \n{0:N2}", PlayerRB.velocity.magnitude);  
+        if (hasAuthority)
+        {
+            speedometerText.text = string.Format("Speed: \n{0:N2}", PlayerRB.velocity.magnitude);
+        }
+          
     }
 }
