3c44f476ba87489bc48b97bdf4c698518a880ef0
foxencore <foxencore@users.noreply.github.com>
2021-02-12 18:04:17 -0600

    reworking levels

---
+++ Assets/Scripts/Level02/Spinners.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinners : MonoBehaviour
+{
+
+    public float speed = 100;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        
+        m_EulerAngleVelocity = new Vector3(0, speed, 0);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+    }
+}
+
+
