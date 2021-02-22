6be9f8b317a594ce5eb793b5d5e9c7bdcdb5a962
foxencore <foxencore@users.noreply.github.com>
2021-02-22 17:02:28 -0600

    Added some of Blakes sfx (not implemented)

--- Assets/Scripts/GoalTriggerEvent.cs
+++ Assets/Scripts/GoalTriggerEvent.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class GoalTriggerEvent : MonoBehaviour
+public class GoalTriggerEvent : NetworkBehaviour
 {
 
     public Transform startPosition;
+    public AudioClip goalSound;
+    
+    private AudioSource audioSource;
+
     //public GameObject Player;
     
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -26,6 +30,10 @@ public class GoalTriggerEvent : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             other.GetComponentInParent<PlayerRespawn>().Respawn();
+            if (hasAuthority)
+            {
+                audioSource.PlayOneShot(goalSound, 0.5f);
+            }
         }
 
 
